26b4ed1ef0e112bbb4b10b27a07a663c464b0836
Larry Georges Muala <private_muala@hotmail.com>
2018-05-02 12:03:11 +0200

    disabling background task

--- market/settings.py
+++ market/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'background_task',
+    #'background_task',
 ]
 
 MIDDLEWARE = [
