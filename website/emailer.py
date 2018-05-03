ea092be5db15ae521b2c7da2e3a67416d056c04e
Larry Georges Muala <private_muala@hotmail.com>
2018-05-03 10:39:22 +0200

    more cleaning

--- website/emailer.py
+++
@@ -1,9 +0,0 @@
-
-##GMAIL Email Configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'Larrygmail789' #my gmail password
-EMAIL_HOST_USER = 'enquiries.dlm@gmail.com' #my gmail username
-SERVER_NOTIFIER = ['enquiries.dlm@gmail.com']
-
-##DJANGO Secret Key
-SECRET_KEY = 'tl#vib3qs=0099ojy^1h14%@dm+c2h3z7j$5u+zdjk74fme1q)'
